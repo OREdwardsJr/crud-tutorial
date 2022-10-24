1e174dc52f9d6188eeaa58e655f3f8a911b16c80
Orlando <oredwardsjr@gmail.com>
2022-10-24 10:53:36 -0700

    deploying

--- db/index.js
+++ db/index.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.DATABASE, {
-    useUnifiedTopology: true,
-    dbName: 'wishlists'
-}).then(() => console.log("Database connected"))
-.catch(e => console.error("Database not connected: ", e));
+// mongoose.connect(process.env.DATABASE, {
+//     useUnifiedTopology: true,
+//     dbName: 'wishlists'
+// }).then(() => console.log("Database connected"))
+// .catch(e => console.error("Database not connected: ", e));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/wishlist',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
 
 const db = mongoose.connection;
 
