f53fd0331af9de5ee290a29d25c448d8a98eeabe
Orlando <oredwardsjr@gmail.com>
2022-10-24 13:54:28 -0700

    deploying

--- routes/destination.js
+++ routes/destination.js
@@ -2,20 +2,26 @@ const express = require("express");
 const router = express.Router();
 const { Destination } = require("./../models");
 const { createDestination, getDestinations, getDestination, deleteDestination, updateDestination } = require("./../controllers/destination-cntrl");
+const herokuLine = 'https://vacation-wishlist.herokuapp.com/'
 
 // create
-router.post('/destination/create', createDestination);
+//router.post('/destination/create', createDestination);
+router.post(herokuLine = '/destination/create', createDestination);
 
 // delete
-router.delete("/destination/delete/:id", deleteDestination);
+//router.delete("/destination/delete/:id", deleteDestination);
+router.delete(herokuLine + "/destination/delete/:id", deleteDestination);
 
 // get by id
-router.get("/destination/:id", getDestination);
+//router.get("/destination/:id", getDestination);
+router.get(herokuLine + "/destination/:id", getDestination);
 
 // get all
-router.get("/destination", getDestinations);
+//router.get("/destination", getDestinations);
+router.get(herokuLine + "/destination", getDestinations);
 
 // update
-router.put("/destination/update/:id", updateDestination);
+//router.put("/destination/update/:id", updateDestination);
+router.put(herokuLine + "/destination/update/:id", updateDestination);
 
 module.exports = router;
