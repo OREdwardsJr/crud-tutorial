58d760401ce9e768d3ed09c9e9d481e348fee57b
Orlando <oredwardsjr@gmail.com>
2022-10-24 20:03:42 -0700

    deploying

--- public/main2.js
+++ public/main2.js
@@ -47,7 +47,7 @@ function activateEditButtons() { // obj is expected to be .container
     let property;
 
     editBtns.forEach(button => {
-        console.log(button);
+        console.log(button.classList.contains(buttonClassPrefix));
         if (button.classList.contains(buttonClassPrefix + "destination")) {
             property = "destination";
         } else if (button.classList.contains(buttonClassPrefix + "location")) {
