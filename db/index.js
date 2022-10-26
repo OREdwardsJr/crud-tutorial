62c6dc751b9ff47917632bfe2a72ec0430d387ba
Orlando <oredwardsjr@gmail.com>
2022-10-26 09:12:42 -0700

    switched to one database with 2 collections

--- db/index.js
+++ db/index.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-const conn = mongoose.createConnection(
+const conn = mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://https://vacation-wishlist.herokuapp.com//wishlist',
   {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       dbName: 'wishlists'
-  });
-  // .then(() => {console.log('database connected')})
-  // .catch(e => console.error("Database not connected: ", e));
+  })
+  .then(() => {console.log('database connected')})
+  .catch(e => console.error("Database not connected: ", e));
 
-conn.model('Wishlists', require('../models/index'));
+const db = mongoose.connection;
 
-module.exports = conn;
+module.exports = db;
